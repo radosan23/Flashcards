5e6abaee691afc509a8b40eacebfdbb4a40ae869
radosan <radoslaw.sanetra@gmail.com>
2023-04-24 19:30:52 +0200

    added logging to file option

--- Flashcards (Python)/task/flashcards/flashcards.py
+++ Flashcards (Python)/task/flashcards/flashcards.py
@@ -1,3 +1,4 @@
+import io
 import json
 import os
 import random
@@ -16,59 +17,73 @@ class DuplicatedData(Exception):
 class Flashcards:
     def __init__(self):
         self.deck = []
+        self.log_buffer = io.StringIO()
+
+    def log_out(self, msg):
+        print(msg, file=self.log_buffer)
+        print(msg)
+
+    def log_in(self, msg):
+        print(msg, file=self.log_buffer, end='')
+        inp = input(msg)
+        print(inp, file=self.log_buffer)
+        return inp
 
     def menu(self):
         while True:
-            cmd = input('Input the action (add, remove, import, export, ask, log, hardest card, reset stats, exit):\n')
+            cmd = self.log_in('Input the action '
+                              '(add, remove, import, export, ask, exit, log, hardest card, reset stats):\n')
             if cmd == 'exit':
-                print('Bye bye!')
+                self.log_out('Bye bye!')
                 break
             elif cmd == 'add':
                 self.add_card()
             elif cmd == 'remove':
-                self.remove_card(input('Which card?\n'))
+                self.remove_card(self.log_in('Which card?\n'))
             elif cmd == 'import':
-                self.import_cards(input('File name:\n'))
+                self.import_cards(self.log_in('File name:\n'))
             elif cmd == 'export':
-                self.export_cards(input('File name:\n'))
+                self.export_cards(self.log_in('File name:\n'))
             elif cmd == 'ask':
-                self.play(int(input('How many times to ask?\n')))
+                self.play(int(self.log_in('How many times to ask?\n')))
             elif cmd == 'log':
-                self.log()
+                self.log(self.log_in('File name:\n'))
             elif cmd == 'hardest card':
                 self.hardest_card()
             elif cmd == 'reset stats':
                 self.reset_stats()
 
     def add_card(self):
-        t = input(f'The card:\n')
+        t = self.log_in(f'The card:\n')
         t = self.check_duplicate('card', t)
-        d = input(f'The definition of the card:\n')
+        d = self.log_in(f'The definition of the card:\n')
         d = self.check_duplicate('definition', d)
         self.deck.append({'term': t, 'definition': d, 'mistakes': 0})
-        print(f'The pair ("{t}":"{d}") has been added.\n')
+        self.log_out(f'The pair ("{t}":"{d}") has been added.\n')
 
     def remove_card(self, card):
         to_remove = [x for x in self.deck if x['term'] == card]
         if to_remove:
             self.deck.remove(to_remove[0])
-            print('The card has been removed.\n')
+            self.log_out('The card has been removed.\n')
         else:
-            print(f'Can\'t remove "{card}": there is no such card.\n')
+            self.log_out(f'Can\'t remove "{card}": there is no such card.\n')
 
     def import_cards(self, file):
         if os.access(file, os.F_OK):
             with open(file, 'rt') as f:
                 imported = json.loads(f.read())
-                self.deck.extend(imported)
-                print(f'{len(imported)} cards have been loaded.\n')
+                for card in imported:
+                    if card not in self.deck:
+                        self.deck.append(card)
+                self.log_out(f'{len(imported)} cards have been loaded.\n')
         else:
-            print('File not found\n')
+            self.log_out('File not found\n')
 
     def export_cards(self, file):
         with open(file, 'wt') as f:
             f.write(json.dumps(self.deck))
-            print(f'{len(self.deck)} cards have been saved.\n')
+            self.log_out(f'{len(self.deck)} cards have been saved.\n')
 
     def check_duplicate(self, d_type, data):
         while True:
@@ -77,42 +92,44 @@ class Flashcards:
                         or d_type == 'definition' and data in [x['definition'] for x in self.deck]:
                     raise DuplicatedData(d_type, data)
             except DuplicatedData as dupl:
-                data = input(dupl)
+                data = self.log_in(dupl)
             else:
                 break
         return data
 
     def play(self, n):
         for card in random.choices(list(self.deck), k=n):
-            ans = input(f"Print the definition of \"{card['term']}\":\n")
+            ans = self.log_in(f"Print the definition of \"{card['term']}\":\n")
             if ans == card['definition']:
-                print('Correct!\n')
+                self.log_out('Correct!\n')
             else:
                 card['mistakes'] += 1
                 if ans in [x['definition'] for x in self.deck]:
                     r_term = list(filter(lambda x: x['definition'] == ans, self.deck))[0]['term']
-                    print(f"Wrong. The right answer is \"{card['definition']}\", "
-                          f"but your definition is correct for \"{r_term}\".\n")
+                    self.log_out(f"Wrong. The right answer is \"{card['definition']}\", "
+                                 f"but your definition is correct for \"{r_term}\".\n")
                 else:
-                    print(f"Wrong. The right answer is \"{card['definition']}\".\n")
+                    self.log_out(f"Wrong. The right answer is \"{card['definition']}\".\n")
 
-    def log(self):
-        pass
+    def log(self, file):
+        with open(file, 'wt') as f:
+            f.write(self.log_buffer.getvalue())
+            self.log_out('The log has been saved.\n')
 
     def hardest_card(self):
-        max_mistakes = max([x['mistakes'] for x in self.deck])
+        max_mistakes = max([x['mistakes'] for x in self.deck]) if self.deck else 0
         if max_mistakes:
             mistaken = ['"' + x['term'] + '"' for x in self.deck if x['mistakes'] == max_mistakes]
             words = ['card is', 'it'] if len(mistaken) == 1 else ['cards are', 'them']
-            print(f'The hardest {words[0]} {", ".join(mistaken)}. '
-                  f'You have {max_mistakes} errors answering {words[1]}.\n')
+            self.log_out(f'The hardest {words[0]} {", ".join(mistaken)}. '
+                         f'You have {max_mistakes} errors answering {words[1]}.\n')
         else:
-            print('There are no cards with errors.\n')
+            self.log_out('There are no cards with errors.\n')
 
     def reset_stats(self):
         for card in self.deck:
             card['mistakes'] = 0
-        print('Card statistics have been reset.\n')
+        self.log_out('Card statistics have been reset.\n')
 
 
 def main():
